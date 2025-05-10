52bb8083f3dbf26f17f2585758034b1a167fc78d
himanshuc11 <himanshuchhatpar@gmail.com>
2025-05-10 11:28:05 +0530

    feat: add Logger utility class for enhanced logging functionality

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import type { Mode } from "../../types/rootpay-secure-payment-sdk";
+
+export class Logger {
+    private mode: Mode;
+    private readonly YELLOW = '\x1b[33m%s\x1b[0m';
+    private readonly RED = '\x1b[31m%s\x1b[0m';
+
+    constructor(mode: Mode) {
+        this.mode = mode;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public logWithoutChecks(...args: any[]): void {
+        console.log(this.YELLOW, args.join(' '));
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public info(...args: any[]): void {
+        if (this.mode !== 'dev') return;
+        console.log(this.YELLOW, args.join(' '));
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public error(...args: any[]): void {
+        if (this.mode !== 'dev') return;
+        console.log(this.RED, args.join(' '));
+    }
+}
